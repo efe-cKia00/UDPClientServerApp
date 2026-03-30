419f870ca8a811bac64ec03abfdbec3461271aeb
Efe Awo-Osagie <deafveid@gmail.com>
2026-03-29 19:49:42 -0500

    Finished implementing Request.java and Response.java

--- src/main/java/org/company/cs455project2/Request.java
+++ src/main/java/org/company/cs455project2/Request.java
@@ -1,5 +1,83 @@
 package org.company.cs455project2;
 
-public class Request {
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Request implements Serializable {
+    private static final long serialVersionUID = 1L;
     
+    private RequestType requestType;
+    private String clientIP;
+    private int clientPort;
+    private long timestamp;
+    private Map<String, String> parameters;
+    
+    /**
+     * Constructor for Request
+     * 
+     * @param requestType the type of request
+     * @param clientIP the IP address of the client
+     * @param clientPort the port of the client
+     */
+    public Request(RequestType requestType, String clientIP, int clientPort) {
+        this.requestType = requestType;
+        this.clientIP = clientIP;
+        this.clientPort = clientPort;
+        this.timestamp = System.currentTimeMillis();
+        this.parameters = new HashMap<>();
+    }
+    
+    // Getters
+    public RequestType getRequestType() {
+        return requestType;
+    }
+    
+    public String getClientIP() {
+        return clientIP;
+    }
+    
+    public int getClientPort() {
+        return clientPort;
+    }
+    
+    public long getTimestamp() {
+        return timestamp;
+    }
+    
+    public Map<String, String> getParameters() {
+        return parameters;
+    }
+    
+    /**
+     * Add a parameter to the request
+     * 
+     * @param key parameter key
+     * @param value parameter value
+     */
+    public void addParameter(String key, String value) {
+        parameters.put(key, value);
+    }
+    
+    /**
+     * Get a parameter value
+     * 
+     * @param key parameter key
+     * @return parameter value or null if not found
+     */
+    public String getParameter(String key) {
+        return parameters.get(key);
+    }
+    
+    @Override
+    public String toString() {
+        return String.format(
+            "Request{type=%s, client=%s:%d, timestamp=%d, params=%s}",
+            requestType,
+            clientIP,
+            clientPort,
+            timestamp,
+            parameters
+        );
+    }
 }
--- src/main/java/org/company/cs455project2/Response.java
+++ src/main/java/org/company/cs455project2/Response.java
@@ -1,5 +1,113 @@
 package org.company.cs455project2;
 
-public class Response {
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Response implements Serializable {
+    private static final long serialVersionUID = 1L;
     
+    private int statusCode;
+    private boolean success;
+    private String message;
+    private Map<String, String> data;
+    private long timestamp;
+    
+    /**
+     * Constructor for Response
+     * 
+     * @param statusCode HTTP-like status code (200, 400, 500, etc.)
+     * @param success whether the request was successful
+     * @param message descriptive message
+     */
+    public Response(int statusCode, boolean success, String message) {
+        this.statusCode = statusCode;
+        this.success = success;
+        this.message = message;
+        this.data = new HashMap<>();
+        this.timestamp = System.currentTimeMillis();
+    }
+    
+    // Getters
+    public int getStatusCode() {
+        return statusCode;
+    }
+    
+    public boolean isSuccess() {
+        return success;
+    }
+    
+    public String getMessage() {
+        return message;
+    }
+    
+    public Map<String, String> getData() {
+        return data;
+    }
+    
+    public long getTimestamp() {
+        return timestamp;
+    }
+    
+    /**
+     * Add data to the response
+     * 
+     * @param key data key
+     * @param value data value
+     */
+    public void addData(String key, String value) {
+        data.put(key, value);
+    }
+    
+    /**
+     * Get data value
+     * 
+     * @param key data key
+     * @return data value or null if not found
+     */
+    public String getData(String key) {
+        return data.get(key);
+    }
+    
+    /**
+     * Factory method for successful response
+     * 
+     * @param message success message
+     * @return Response object with status 200
+     */
+    public static Response success(String message) {
+        return new Response(Constants.SUCCESS, true, message);
+    }
+    
+    /**
+     * Factory method for bad request response
+     * 
+     * @param message error message
+     * @return Response object with status 400
+     */
+    public static Response badRequest(String message) {
+        return new Response(Constants.BAD_REQUEST, false, message);
+    }
+    
+    /**
+     * Factory method for server error response
+     * 
+     * @param message error message
+     * @return Response object with status 500
+     */
+    public static Response serverError(String message) {
+        return new Response(Constants.SERVER_ERROR, false, message);
+    }
+    
+    @Override
+    public String toString() {
+        return String.format(
+            "Response{status=%d, success=%b, message='%s', data=%s, timestamp=%d}",
+            statusCode,
+            success,
+            message,
+            data,
+            timestamp
+        );
+    }
 }
